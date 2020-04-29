972dd7bd43be06cc52d96cad55c549c2b3b4a3c4
chenxuehua0 <chenxuehua0@163.com>
2020-04-29 23:43:44 +0800

    修改样式

--- jingwei-task-monitor/src/main/java/Test.java
+++ jingwei-task-monitor/src/main/java/Test.java
@@ -4,6 +4,6 @@ import org.sxdata.jingwei.util.TaskUtil.KettleEncr;
 public class Test {
     public static void main(String[] args) {
 //        Encr.decryptPassword
-        System.out.println(KettleEncr.encryptPassword("root123456ABCD!@#"));
+        System.out.println(KettleEncr.encryptPassword("sjjr@2020A"));
     }
 }
