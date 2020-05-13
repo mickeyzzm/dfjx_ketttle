8b74744d4aadcb83820a41e3590de6a7b2e83a68
chenxuehua0 <chenxuehua0@163.com>
2020-05-13 16:49:02 +0800

    放开菜单树功能

--- kettle-ext/src/main/java/org/flhy/ext/trans/TransExecutionConfigurationCodec.java
+++ kettle-ext/src/main/java/org/flhy/ext/trans/TransExecutionConfigurationCodec.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.xml.parsers.ParserConfigurationException;
 
